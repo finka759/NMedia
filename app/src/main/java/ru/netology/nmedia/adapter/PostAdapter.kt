f8c400277871e4505aa6b54ba1ae765100e65856
finka759 <finka759@gmail.com>
2025-06-11 22:35:28 +0300

    deleted not using elements

--- app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
@@ -2,7 +2,6 @@ package ru.netology.nmedia.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ListView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
