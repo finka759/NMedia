7e0d51a55db29bc3e542024e18a8a84aa28661eb
finka759 <finka759@gmail.com>
2025-08-10 21:45:04 +0300

    adeded Room

---
+++ app/src/main/java/ru/netology/nmedia/entity/PostEntity.kt
@@ -0,0 +1,46 @@
+package ru.netology.nmedia.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.netology.nmedia.dto.Post
+
+@Entity(tableName = "Post_Entity")
+data class PostEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val author: String,
+    val published: String,
+    val content: String,
+    val likeCount: Int = 0,
+    val likeByMe: Boolean = false,
+    val shareCount: Int = 0,
+    val viewCount: Int = 0,
+    val videoUrl: String? = null,
+) {
+    fun toDto() = Post(
+        id = id,
+        author = author,
+        published = published,
+        content = content,
+        likeCount = likeCount,
+        likeByMe = likeByMe,
+        shareCount = shareCount,
+        viewCount = viewCount,
+        videoUrl = videoUrl,
+    )
+
+    companion object {
+        fun fromDto(dto: Post) =
+            PostEntity(
+                id = dto.id,
+                author = dto.author,
+                published = dto.published,
+                content = dto.content,
+                likeCount = dto.likeCount,
+                likeByMe = dto.likeByMe,
+                shareCount = dto.shareCount,
+                viewCount = dto.viewCount,
+                videoUrl = dto.videoUrl,
+            )
+    }
+}
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositorySQLiteImp.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositorySQLiteImp.kt
@@ -1,66 +1,66 @@
-package ru.netology.nmedia.repository
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import ru.netology.nmedia.dao.PostDao
-import ru.netology.nmedia.dto.Post
-
-class PostRepositorySQLiteImpl(
-    private val dao: PostDao
-) : PostRepository {
-    private var posts = emptyList<Post>()
-    private val data = MutableLiveData(posts)
-
-    init {
-        posts = dao.getAll()
-        data.value = posts
-    }
-
-    override fun share(id: Long) {
-        dao.shareById(id)
-        posts = posts.map { post ->
-            if (post.id == id) {
-                post.copy(
-                    shareCount = post.shareCount + 1
-                )
-            } else {
-                post
-            }
-        }
-        data.value = posts
-    }
-
-
-    override fun get(): LiveData<List<Post>> = data
-
-
-    override fun save(post: Post) {
-        val id = post.id
-        val saved = dao.save(post)
-        posts = if (id == 0L) {
-            listOf(saved) + posts
-        } else {
-            posts.map {
-                if (it.id != id) it else saved
-            }
-        }
-        data.value = posts
-    }
-
-    override fun like(id: Long) {
-        dao.likeById(id)
-        posts = posts.map {
-            if (it.id != id) it else it.copy(
-                likeByMe = !it.likeByMe,
-                likeCount = if (it.likeByMe) it.likeCount - 1 else it.likeCount + 1
-            )
-        }
-        data.value = posts
-    }
-
-    override fun removeById(id: Long) {
-        dao.removeById(id)
-        posts = posts.filter { it.id != id }
-        data.value = posts
-    }
-}
+//package ru.netology.nmedia.repository
+//
+//import androidx.lifecycle.LiveData
+//import androidx.lifecycle.MutableLiveData
+//import ru.netology.nmedia.dao.PostDao
+//import ru.netology.nmedia.dto.Post
+//
+//class PostRepositorySQLiteImpl(
+//    private val dao: PostDao
+//) : PostRepository {
+//    private var posts = emptyList<Post>()
+//    private val data = MutableLiveData(posts)
+//
+//    init {
+//        posts = dao.getAll()
+//        data.value = posts
+//    }
+//
+//    override fun share(id: Long) {
+//        dao.shareById(id)
+//        posts = posts.map { post ->
+//            if (post.id == id) {
+//                post.copy(
+//                    shareCount = post.shareCount + 1
+//                )
+//            } else {
+//                post
+//            }
+//        }
+//        data.value = posts
+//    }
+//
+//
+//    override fun get(): LiveData<List<Post>> = data
+//
+//
+//    override fun save(post: Post) {
+//        val id = post.id
+//        val saved = dao.save(post)
+//        posts = if (id == 0L) {
+//            listOf(saved) + posts
+//        } else {
+//            posts.map {
+//                if (it.id != id) it else saved
+//            }
+//        }
+//        data.value = posts
+//    }
+//
+//    override fun like(id: Long) {
+//        dao.likeById(id)
+//        posts = posts.map {
+//            if (it.id != id) it else it.copy(
+//                likeByMe = !it.likeByMe,
+//                likeCount = if (it.likeByMe) it.likeCount - 1 else it.likeCount + 1
+//            )
+//        }
+//        data.value = posts
+//    }
+//
+//    override fun removeById(id: Long) {
+//        dao.removeById(id)
+//        posts = posts.filter { it.id != id }
+//        data.value = posts
+//    }
+//}
