1d37a236de141be8995baeb8017cf0c43bb88b50
finka759 <finka759@gmail.com>
2025-08-22 22:20:10 +0300

    Notifications & Pushes

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -1,13 +1,20 @@
 package ru.netology.nmedia.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.ActivityAppBinding
 import androidx.navigation.findNavController
 import ru.netology.nmedia.fragments.FeedFragment.Companion.textArgs
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.firebase.messaging.FirebaseMessaging
 
 
 class AppActivity : AppCompatActivity() {
@@ -15,6 +22,7 @@ class AppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityAppBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestNotificationsPermission()
 
         intent?.let {
             if (it.action != Intent.ACTION_SEND) return@let
@@ -42,5 +50,39 @@ class AppActivity : AppCompatActivity() {
 
         }
 
+        checkGoogleApiAvailability()
+
     }
+
+    private fun requestNotificationsPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            return
+        }
+
+        val permission = Manifest.permission.POST_NOTIFICATIONS
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        requestPermissions(arrayOf(permission), 1)
+    }
+
+    private fun checkGoogleApiAvailability() {
+        with(GoogleApiAvailability.getInstance()) {
+            val code = isGooglePlayServicesAvailable(this@AppActivity)
+            if (code == ConnectionResult.SUCCESS) {
+                return@with
+            }
+            if (isUserResolvableError(code)) {
+                getErrorDialog(this@AppActivity, code, 9000)?.show()
+                return
+            }
+            Toast.makeText(this@AppActivity, "___R.string.google_play_unavailable___", Toast.LENGTH_LONG).show()
+        }
+
+        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            println(it)
+        }
+    }
+
 }
---
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -0,0 +1,139 @@
+package ru.netology.nmedia.service
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.gson.Gson
+import ru.netology.nmedia.R
+import kotlin.random.Random
+
+class FCMService : FirebaseMessagingService() {
+
+    private val action = "action"
+    private val content = "content"
+    private val channelId = "remote"
+    private val gson = Gson()
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        Log.i("---fsm-token---", token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+//        Log.i("---fsm-message-geted---", message.data.toString())
+        try {
+            message.data[action]?.let {
+                when (Action.valueOf(it)) {
+                    Action.LIKE -> handleLike(
+                        gson.fromJson(message.data[content], ActionLike::class.java)
+                    )
+                    Action.NEW_POST -> handleNewPost(
+                        gson.fromJson(message.data[content], NewPost::class.java)
+                    )
+                }
+            }
+        } catch (e: IllegalArgumentException) {
+            Log.i("---fsm-message-geted---", message.data.toString())
+            println(e.message)
+            for (line in e.stackTrace) {
+                println("at $line")
+                Log.i("---error---", "at $line")
+            }
+        }
+
+//        Log.i("---fsm-message---", message.notification?.body.toString())
+    }
+
+    private fun handleLike(content: ActionLike) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_liked,
+                    content.userName,
+                    content.postAuthor,
+                )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        NotificationManagerCompat.from(this).notify(Random.nextInt(100_000), notification)
+    }
+
+
+    private fun handleNewPost(content: NewPost) {
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_new_post,
+                    content.postAuthor
+                )
+            )
+            .setContentText(content.postTopic)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(content.postText)
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
+    }
+
+
+}
+
+
+enum class Action {
+    LIKE, NEW_POST,
+}
+
+data class ActionLike(
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postAuthor: String,
+)
+
+data class NewPost(
+    val postAuthor: String,
+    val postText: String,
+    val postTopic: String
+)
